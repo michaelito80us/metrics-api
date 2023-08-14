5a9a4fb03d686b9f236dabf3ca21145b13f01270
Michael Epelboim <mepelboim@outlook.com>
2023-08-14 12:16:00 +0200

    comment cleanup

--- app/controllers/api/v1/metrics_controller.rb
+++ app/controllers/api/v1/metrics_controller.rb
@@ -14,7 +14,7 @@ class Api::V1::MetricsController < ApplicationController
   # Retrieves the unique list of metric names
   def metric_list
     names = Metric.pluck(:name).uniq
-    render json: { metric_list: names }
+    render json: { metric_list: names }, status: :ok
   end
 
   # POST /api/v1/averages
@@ -33,16 +33,6 @@ class Api::V1::MetricsController < ApplicationController
     averages = Metric.calculate_averages(metric_params[:metric], start_time, end_time, user_timezone)
 
     render json: averages, status: :ok
-
-    # # Fetching the metric entries based on name and timestamp range
-    # metric_entries = Metric.where(name: metric_params[:metric].downcase,
-    #                               timestamp: start_time..end_time).order(timestamp: :asc)
-
-    # # Calculate the averages for all three intervals
-    # averages = calculate_averages(metric_entries, user_timezone)
-
-    # # Render the results in the JSON response
-    # render json: averages, status: :ok
   end
 
   private
@@ -61,51 +51,4 @@ class Api::V1::MetricsController < ApplicationController
 
     [start_time, end_time]
   end
-
-  # def calculate_averages(metric_entries, user_timezone)
-  #   {
-  #     per_minute: average_by_interval(metric_entries, :minute, user_timezone),
-  #     per_hour: average_by_interval(metric_entries, :hour, user_timezone),
-  #     per_day: average_by_interval(metric_entries, :day, user_timezone)
-  #   }
-  # end
-
-  # def group_by_interval(metric_data, interval)
-  #   metric_data.group_by do |entry|
-  #     case interval
-  #     when :minute then entry.timestamp.beginning_of_minute
-  #     when :hour then entry.timestamp.beginning_of_hour
-  #     when :day then entry.timestamp.beginning_of_day
-  #     end
-  #   end
-  # end
-
-  # def format_key(key, interval, user_timezone)
-  #   case interval
-  #   when :minute then key.in_time_zone(user_timezone).strftime('%Y-%m-%d %H:%M')
-  #   when :hour then key.in_time_zone(user_timezone).strftime('%Y-%m-%d %H:00')
-  #   when :day then key.in_time_zone(user_timezone).strftime('%Y-%m-%d')
-  #   end
-  # end
-
-  # def averages_calculation(entries)
-  #   total_value = entries.sum(&:value)
-  #   avg_value = total_value / entries.size.to_f
-  #   format('%.1f', avg_value).to_f
-  # end
-
-  # # Averages the given metric data by the specified interval (:minute, :hour, or :day)
-  # # Returns a hash where keys are the beginning of the intervals and values are the calculated averages
-  # def average_by_interval(metric_data, interval, user_timezone)
-  #   interval_data = group_by_interval(metric_data, interval)
-
-  #   # Convert the keys to the user's time zone and format them
-  #   formatted_interval_data = interval_data.transform_keys do |key|
-  #     format_key(key, interval, user_timezone)
-  #   end
-
-  #   formatted_interval_data.transform_values do |entries|
-  #     averages_calculation(entries)
-  #   end
-  # end
 end
